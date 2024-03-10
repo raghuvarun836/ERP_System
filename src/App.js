f5c8b05387a2beafed7d36aef9c990b2d32744fd
Raghu <raghuvarun836@gmail.com>
2024-03-10 09:51:36 +0530

    made changes in the nav bars

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Products from "./components/Products";
 import Orders from "./components/Orders";
 import OrdersCalendarView from "./components/OrdersCalendarView";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
+        <nav className="nav">
           <ul>
             <li>
-              <Link to="/">Dashboard</Link>
+              <NavLink to="/" activeclassname="active-link" exact="true">
+                Dashboard
+              </NavLink>
             </li>
             <li>
-              <Link to="/products">Products</Link>
+              <NavLink to="/products" activeclassname="active-link">
+                Products
+              </NavLink>
             </li>
             <li>
-              <Link to="/orders">Orders</Link>
+              <NavLink to="/orders" activeclassname="active-link">
+                Orders
+              </NavLink>
             </li>
             <li>
-              <Link to="/calendar">Calendar</Link>
+              <NavLink to="/calendar" activeclassname="active-link">
+                Calendar
+              </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/" exact element={<Dashboard/>} />
-          <Route path="/products" element={<Products/>} />
-          <Route path="/orders" element={<Orders/>} />
-          <Route path="/calendar" element={<OrdersCalendarView/>} />
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/calendar" element={<OrdersCalendarView />} />
         </Routes>
       </div>
     </Router>
